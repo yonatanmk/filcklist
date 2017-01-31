c6c8b78b6d53c93b4188cc5ed4632128d339ba8c
Yonatan MK <yonatanmk@gmail.com>
2017-01-31 15:22:27 -0500

    added destroy userMovie tests

--- spec/controllers/api/v1/user_movies_controller_spec.rb
+++ spec/controllers/api/v1/user_movies_controller_spec.rb
@@ -41,4 +41,34 @@ RSpec.describe Api::V1::UserMoviesController, type: :controller do
       expect(UserMovie.first.status).to eq('dislike')
     end
   end
+  describe "DELETE #destroy" do
+    let(:tj) {
+      User.create(
+        email: "tjdetweiler@recess.com",
+        username: "TJ",
+        password: "123456"
+      )
+    }
+    let(:batman) {
+      Movie.create(
+        id: 268,
+        title: "Batman",
+        poster_path: "/kBf3g9crrADGMc2AMAMlLBgSm2h.jpg",
+        release_date: "06/23/1989",
+        overview:
+        "The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker, who has seized control of Gotham's underworld.",
+        adult: false,
+        status: nil
+      )
+    }
+    it "should delete a UserMovie" do
+      UserMovie.create(user: tj, movie: batman)
+      expect(UserMovie.first.status).to eq('seen')
+      get :create, params: { user_movie: {user_id: tj.id, movie_id: batman.id, status: 'want' }}
+      expect(UserMovie.all.length).to eq(1)
+      expect(UserMovie.first.status).to eq('want')
+      get :destroy, params: { id: 1, user_movie: {user_id: tj.id, movie_id: batman.id}}
+      expect(UserMovie.all.length).to eq(0)
+    end
+  end
 end
