1a15c263e98f8419fc4a79c88ad0f87390abc697
Yonatan MK <yonatanmk@gmail.com>
2017-01-31 14:55:06 -0500

    functional delete button

--- react/src/components/IndexMovieBox.js
+++ react/src/components/IndexMovieBox.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 import * as api from '../api';
 import { addUserMovie } from '../actions';
 
-const IndexMovieBox = ({movie, movies, user, userMovies, page, handleButtonClick}) => {
+const IndexMovieBox = ({movie, movies, user, userMovies, page, handleButtonClick, handleDeleteButtonClick}) => {
   let className;
   if (page === "index") {
     className = "small-6 medium-4 large-2 columns index-box";
@@ -81,6 +81,15 @@ const IndexMovieBox = ({movie, movies, user, userMovies, page, handleButtonClick
     handleButtonClick(user, movie, event.target.value);
   };
 
+  let onDeleteButtonClick = () => {
+    handleDeleteButtonClick(user, movie);
+  };
+
+  let deleteButton;
+  if (userMovie) {
+    deleteButton = <button onClick={onDeleteButtonClick}>Remove Movie</button>;
+  }
+
   return (
     <div key={movie.id} className={className}>
       {boxHeader}
@@ -90,6 +99,7 @@ const IndexMovieBox = ({movie, movies, user, userMovies, page, handleButtonClick
       <button onClick={onButtonClick} value='add'>Already Seen</button>
       <button onClick={onButtonClick} value='like'>Like This Movie</button>
       <button onClick={onButtonClick} value='dislike'>Dislike This Movie</button>
+      {deleteButton}
       <div className='index-image'>
         <img src={image_url} />
       </div>
