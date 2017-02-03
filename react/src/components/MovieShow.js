56b32cf319186d6944f87ea5aaffb3a1dd3a0b22
Yonatan MK <yonatanmk@gmail.com>
2017-02-03 13:55:38 -0500

    styled show page

--- react/src/components/MovieShow.js
+++ react/src/components/MovieShow.js
@@ -27,9 +27,15 @@ class MovieShow extends Component {
       return(
         <div className='show-page'>
           <h1>{movie.title}</h1>
-          <img src={`http://image.tmdb.org/t/p/w185/${movie.poster_path}`} />
-          <p>Release Date: {movie.release_date}</p>
-          <p>{movie.overview}</p>
+          <div className='row'>
+            <div className='small-6 columns'>
+              <h5>Release Date: {movie.release_date}</h5>
+              <p>{movie.overview}</p>
+            </div>
+            <div className='small-6 columns'>
+              <img src={`http://image.tmdb.org/t/p/w300/${movie.poster_path}`} />
+            </div>
+          </div>
         </div>
       );
     } else {
