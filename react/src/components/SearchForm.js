212d45e8f8fa2516f3b0a98cb96a01f7d3504a45
Yonatan MK <yonatanmk@gmail.com>
2017-02-02 20:29:35 -0500

    devise pages css

--- react/src/components/SearchForm.js
+++ react/src/components/SearchForm.js
@@ -3,14 +3,16 @@ import React from 'react';
 const SearchForm = ({handleSearchFormSubmit}) => {
   let input;
   return (
-    <div className='search-form inline small-6 small-centered columns'>
-      <input className= 'index-search-bar inline' type='text' ref={node => {input = node;}} placeholder='Please Enter A Movie'/>
-      <button className='button inline' onClick={() => {
-        handleSearchFormSubmit(input.value);
-        input.value = '';
-      }}>
-        Find Movie
-      </button>
+    <div className='small-6 small-centered columns'>
+      <div className='search-form inline'>
+        <input className= 'index-search-bar inline' type='text' ref={node => {input = node;}} placeholder='Please Enter A Movie'/>
+        <button className='button inline' onClick={() => {
+          handleSearchFormSubmit(input.value);
+          input.value = '';
+        }}>
+          Find Movie
+        </button>
+      </div>
     </div>
   );
 };
