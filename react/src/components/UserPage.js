28c12fa85709defad81812a5a5fc4eea9a51446e
Yonatan MK <yonatanmk@gmail.com>
2017-02-05 18:51:44 -0500

    reformatted own user profile

--- react/src/components/UserPage.js
+++ react/src/components/UserPage.js
@@ -2,42 +2,54 @@ import React from 'react';
 import { Link } from 'react-router';
 import IndexMovieBoxContainer from '../containers/IndexMovieBoxContainer';
 
-const UserPage = ({user}) => {
-  let wantMovies, seenMovies;
+const UserPage = ({user, profileStatus, handleProfileButtonClick}) => {
+  let handleClick = (event) => {
+    handleProfileButtonClick(event.target.value);
+  };
+
+  let movies;
   if (user) {
-    wantMovies = user.movies.filter((movie)=>{return movie.status === 'want';})
-    .map((movie) => {
+    if (profileStatus === 'seen') {
+      let seenMovies = user.movies.filter((movie)=>{return movie.status === 'seen';});
+      let likedMovies = user.movies.filter((movie)=>{return movie.status === 'like';});
+      let dislikedMovies = user.movies.filter((movie)=>{return movie.status === 'dislike';});
+      movies = seenMovies.concat(likedMovies).concat(dislikedMovies);
+    }
+    else {
+      movies = user.movies.filter((movie)=>{return movie.status === profileStatus;});
+    }
+    movies = movies.map((movie) => {
       return (
         <IndexMovieBoxContainer
           key={movie.id}
           movie={movie}
-          movies={user.movies}
-          page="user"
-        />
-      );
-    });
-    seenMovies = user.movies.filter((movie)=>{return movie.status !== 'want';})
-    .map((movie) => {
-      return (
-        <IndexMovieBoxContainer
-          key={movie.id}
-          movie={movie}
-          movies={user.movies}
+          movies={movies}
           page="user"
         />
       );
     });
   }
+
+
   return (
     <div>
-      <h1>Your Movies</h1>
-      <div className='small-6 columns'>
-        <h3>Plan to Watch</h3>
-        {wantMovies}
+      <div className='content-box user-header'>
+        <h1>Your Movies</h1>
+        <button className='user-button' onClick={handleClick} value='want'>
+          Plan to Watch
+        </button>
+        <button className='user-button' onClick={handleClick} value='like'>
+          Liked Movies
+        </button>
+        <button className='user-button' onClick={handleClick} value='dislike'>
+          Disliked Movies
+        </button>
+        <button className='user-button' onClick={handleClick} value='seen'>
+          All Seen Movies
+        </button>
       </div>
-      <div className='small-6 columns'>
-        <h3>Seen Movies</h3>
-        {seenMovies}
+      <div className='small-12 columns'>
+        {movies}
       </div>
     </div>
   );
--- react/src/containers/UserPageContainer.js
+++ react/src/containers/UserPageContainer.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import UserPage from '../components/UserPage';
+import { setProfileStatusAction } from '../actions';
+
 
 const mapStateToProps = (state) => {
   return {
-    user: state.user
+    user: state.user,
+    profileStatus: state.profileStatus
   };
 };
 
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     handleMovieClick: (movie) => {
-//       dispatch(addMovie(movie));
-//       dispatch(setCurrentMovie(movie));
-//     }
-//   };
-// };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleProfileButtonClick: (status) => {
+      dispatch(setProfileStatusAction(status));
+    }
+  };
+};
 
-const UserPageContainer = connect(mapStateToProps)(UserPage);
+const UserPageContainer = connect(mapStateToProps, mapDispatchToProps)(UserPage);
 
 
 export default UserPageContainer;
