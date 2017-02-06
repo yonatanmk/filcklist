03fa181fdc3d9cad80e335ff01b32d53ae40f6e7
Yonatan MK <yonatanmk@gmail.com>
2017-02-06 12:26:58 -0500

    movies_controller now handles creating actors and movie_actors

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::MoviesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def index
-    @data = get_movie_db_info(params[:query])
+    @data = get_movie_db_movie_info(params[:query])
     render json: @data
   end
 
@@ -15,7 +15,17 @@ class Api::V1::MoviesController < ApplicationController
     @movie = Movie.new(movie_params)
     @movie.release_date = "#{@movie.release_date[5..6]}/#{@movie.release_date[8..9]}/#{@movie.release_date[0..3]}"
     if Movie.where("id = #{@movie.id}").length == 0
-      @movie.save
+      if @movie.save
+        @cast = get_movie_db_cast_info(@movie.id)
+        @cast[0..5].each do |actor|
+          @actor = Actor.new(id: actor['id'], name: actor['name'], profile_path: actor['profile_path'])
+          if Actor.where("id = #{@actor.id}").length == 0
+            @actor.save
+          end
+          MovieActor.create()
+          MovieActor.create(actor: @actor, movie: @movie, character: actor['character'])
+        end
+      end
     end
     render json: {}
   end
@@ -32,13 +42,22 @@ class Api::V1::MoviesController < ApplicationController
     params.require(:movie).permit(:id, :title, :poster_path, :release_date, :overview, :adult)
   end
 
-  def movie_db_uri(query)
+  def movie_db_movie_uri(query)
     query = query.split(' ').join('+')
     URI("https://api.themoviedb.org/3/search/movie?query=#{query}&api_key=#{ENV["MOVIE_DB_API_KEY"]}")
   end
 
-  def get_movie_db_info(query)
-    response = Net::HTTP.get_response(movie_db_uri(query))
+  def get_movie_db_movie_info(query)
+    response = Net::HTTP.get_response(movie_db_movie_uri(query))
     return JSON.parse(response.body)['results']
+  end
+
+  def movie_db_cast_uri(id)
+    URI("https://api.themoviedb.org/3/movie/#{id}/casts?api_key=#{ENV["MOVIE_DB_API_KEY"]}")
+  end
+
+  def get_movie_db_cast_info(id)
+    response = Net::HTTP.get_response(movie_db_cast_uri(id))
+    return JSON.parse(response.body)['cast']
   end
 end
