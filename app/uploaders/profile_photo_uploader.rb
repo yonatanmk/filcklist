57b37780e1322afbe5fd608859b292a0ab0637cc
Yonatan MK <yonatanmk@gmail.com>
2017-02-07 13:06:28 -0500

    profile upload using s3

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -5,12 +5,12 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # if Rails.env.test?
-  #   storage :file
-  # else
-  #   storage :fog
-  # end
-  storage :file
+  if Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
