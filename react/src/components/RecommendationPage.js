ef3fddb9a2b854c9754694f557fc48b34b809ff7
Yonatan MK <yonatanmk@gmail.com>
2017-02-07 18:08:07 -0500

    change navbar profile photo hover

--- react/src/components/RecommendationPage.js
+++ react/src/components/RecommendationPage.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const RecommendationPage = ({userMovies}) => {
 
-  if (userMovies && userMovies.length == 0) {
+  if (!userMovies) {
+    return <div></div>;
+  }
+  else if (userMovies.length < 5) {
     return (
       <div className='darker-content-box not-found'>
-        <h3>You Must Have Liked Movies On Your Profile Before You Can Receive Movie Recommendations</h3>
+        <h3>You Must Have At Least 5 Liked Movies On Your Profile Before You Can Receive Movie Recommendations</h3>
       </div>
     );
   }
