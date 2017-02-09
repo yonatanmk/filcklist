aff0f25da5148a1c08d402c4ac5bc3ed30c3a561
Yonatan MK <yonatanmk@gmail.com>
2017-02-09 16:50:52 -0500

    recaptcha present but not working

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
   prepend_before_action :check_captcha, only: [:create]
 
+  def create
+    binding.pry
+    if !verify_recaptcha
+      flash.delete :recaptcha_error
+      build_resource(sign_up_params)
+      resource.valid?
+      resource.errors.add(:base, "There was an error with the recaptcha code below. Please re-enter the code.")
+      clean_up_passwords(resource)
+      respond_with_navigational(resource) { render_with_scope :new }
+    else
+      flash.delete :recaptcha_error
+      super
+    end
+  end
+
  private
     def check_captcha
       unless verify_recaptcha
